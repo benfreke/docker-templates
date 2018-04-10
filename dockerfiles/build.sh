48cbbf0b25330e2947a957b16e94d6607e6a4647
Ben Freke <benfreke@gmail.com>
2018-04-11 06:56:23 +1000

    Added Composer Dockerfile Improved build script

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+if [ ! $# -eq 3 ]
+  then
+    echo "3 arguments required"
+    echo "Directory 1 i.e. nginx"
+    echo "Directory 2 i.e. laravel"
+    echo "Tag i.e. 1.0"
+    exit 1;
+fi
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -t "benfreke/$1-$2:$3" -t benfreke/$1-$2:latest ./$1/$2
