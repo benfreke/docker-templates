6f4cfa87fde7085751e9cbc6443a50ea63af91f6
Ben Freke <benfreke@users.noreply.github.com>
2019-11-02 21:42:43 +1000

    Better comments for code

--- dockerfiles/php/entrypoint.sh
+++ dockerfiles/php/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# turn off xdebug if we don't want it
+# switch for xdebug
 if [ "$XDEBUG_ENABLED" -eq "1" ]; then
     sed -i "s/xdebug\.remote_enable.*/xdebug\.remote_enable\=1/g" /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 else
     sed -i "s/xdebug\.remote_enable.*/xdebug\.remote_enable\=0/g" /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 fi
 
+# Quick and dirty hack to ensure both .env and .env.example have the same keys
 SAME=$(php -r 'echo count(array_diff_key(parse_ini_file("/var/www/html/.env", false, INI_SCANNER_RAW),  parse_ini_file("/var/www/html/.env.example", false, INI_SCANNER_RAW))) ? "0" : "1";')
 
 # run PHP-FPM in the foreground, as root
